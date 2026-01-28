d004400b37909be0e78b3780e6242328dc16744a
laneri <aneriluis@gmail.com>
2026-01-28 23:09:46 +0100

    initial repo

---
+++ src/main/java/com/raccoon/FlatAdvisor/FlatAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.raccoon.FlatAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raccoon/FlatAdvisor/FlatAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raccoon.FlatAdvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
